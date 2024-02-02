02409d9d5747be7833cd205759aa201732befc6b
Kapil619 <kapilbadokar321@gmail.com>
2024-02-02 16:45:37 +0530

    Added components on HomeScreen

--- app/index.js
+++ app/index.js
@@ -1,11 +1,40 @@
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { COLORS, icons, images, SIZES, FONTS } from '../constants'
+import { Stack, useRouter } from 'expo-router';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 const Home = () => {
-    return (
 
-        <View>
-            <Text>Hey I'm Mohanish Desale , I'm 20 years  old</Text>
-        </View>
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }} >
+            <Stack.Screen options={{
+                headerStyle: {
+                    backgroundColor: COLORS.lightWhite,
+                },
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.search} dimension='60%' />
+                ),
+                headerTitle: ''
+            }} />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
