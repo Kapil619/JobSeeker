0a1021057f5c14bcb0d1bf0f8f0e606439106208
Kapil619 <kapilbadokar321@gmail.com>
2024-02-06 00:10:02 +0530

    Add expo-font plugin and commented splash screen code for layout file

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,27 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text } from 'react-native';
 
+
+// SplashScreen.preventAutoHideAsync()
 const Layout = () => {
+    // const [fontsloaded] = useFonts({
+    //     DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    //     DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    //     DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    // })
+
+    // const onLayoutRootView = useCallback(async () => {
+    //     if (fontsloaded) {
+    //         await SplashScreen.hideAsync()
+    //     }
+    // }, [fontsloaded])
+
+    // if (!fontsloaded) {
+    //     return <Text>Loading...</Text>
+    // }
     return <Stack />;
 }
 
