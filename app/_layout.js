68a4a8c9aa2b7990a8237d75c73a63923568b72d
Kapil619 <kapilbadokar321@gmail.com>
2024-02-06 17:23:22 +0530

    Font loading issue fixed

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,17 @@
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback } from 'react';
+// import * as SplashScreen from 'expo-splash-screen';
+// import { useCallback } from 'react';
 
 
-SplashScreen.preventAutoHideAsync()
+// SplashScreen.preventAutoHideAsync()
+
+
+export const unstable_settings = {
+    // Ensure any route can link back to `/`
+    initialRouteName: "home",
+};
+
 const Layout = () => {
     const [fontsLoaded] = useFonts({
         DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
@@ -12,16 +19,22 @@ const Layout = () => {
         DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
     })
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync()
-        }
-    }, [fontsLoaded])
-
     if (!fontsLoaded) {
         return null;
     }
-    return <Stack onLayout={onLayoutRootView} />;
+
+    // const onLayoutRootView = useCallback(async () => {
+    //     if (fontsLoaded) {
+    //         await SplashScreen.hideAsync()
+    //     }
+    // }, [fontsLoaded])
+
+    // if (!fontsLoaded) {
+    //     return null;
+    // }
+    return <Stack initialRouteName='home'>
+        <Stack.Screen name='home' />
+    </Stack>
 }
 
 export default Layout;
